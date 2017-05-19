befce3738540d42ae1b00988dc83c3d9015f1ece
Kyle <kyle.adams276@gmail.com>
2017-05-19 09:50:24 -0600

    Added user input support

--- MyGreatGame/Controller/Halo6.cs
+++ MyGreatGame/Controller/Halo6.cs
@@ -18,6 +18,16 @@ namespace Halo6.Controller
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
 		private Player player;
+// Keyboard states used to determine key presses
+private KeyboardState currentKeyboardState;
+private KeyboardState previousKeyboardState;
+
+// Gamepad states used to determine button presses
+private GamePadState currentGamePadState;
+private GamePadState previousGamePadState;
+
+// A movement speed for the player
+private float playerMoveSpeed;
 
 		// Initialize the player class
 
@@ -37,10 +47,41 @@ namespace Halo6.Controller
 		protected override void Initialize()
 		{
 			player = new Player();
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
 
 			base.Initialize();
 		}
 
+private void UpdatePlayer(GameTime gameTime)
+	{
+
+	// Get Thumbstick Controls
+	player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+	player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+	// Use the Keyboard / Dpad
+	if (currentKeyboardState.IsKeyDown(Keys.Left) || currentGamePadState.DPad.Left == ButtonState.Pressed)
+		{
+		player.Position.X -= playerMoveSpeed;
+		}
+	if (currentKeyboardState.IsKeyDown(Keys.Right) || currentGamePadState.DPad.Right == ButtonState.Pressed)
+		{
+		player.Position.X += playerMoveSpeed;
+		}
+	if (currentKeyboardState.IsKeyDown(Keys.Up) || currentGamePadState.DPad.Up == ButtonState.Pressed)
+		{
+		player.Position.Y -= playerMoveSpeed;
+		}
+	if (currentKeyboardState.IsKeyDown(Keys.Down) || currentGamePadState.DPad.Down == ButtonState.Pressed)
+		{
+		player.Position.Y += playerMoveSpeed;
+		}
+
+	// Make sure that the player does not go out of bounds
+	player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+	player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+}
 		/// <summary>
 		/// LoadContent will be called once per game and is the place to load
 		/// all of your content.
@@ -71,7 +112,17 @@ player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
 				Exit();
 #endif
 
-			// TODO: Add your update logic here
+// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+previousGamePadState = currentGamePadState;
+previousKeyboardState = currentKeyboardState;
+
+// Read the current state of the keyboard and gamepad and store it
+currentKeyboardState = Keyboard.GetState();
+currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+//Update the player
+UpdatePlayer(gameTime);
 
 			base.Update(gameTime);
 		}
